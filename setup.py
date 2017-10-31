62c5194500451f16c588e2f9a83f389d49b536f9
petervaro <hello@petervaro.com>
2017-10-31 13:07:05 +0000

    pip release setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name         = 'pcd',
+      packages     = ['pcd'],
+      version      = '0.1.0',
+      description  = 'Python Contract Decorators',
+      author       = 'Peter Varo',
+      author_email = 'hello@petervaro.com',
+      url          = 'https://github.com/petervaro/pcd',
+      download_url = 'https://github.com/petervaro/pcd/archive/0.1.0.tar.gz',
+      keywords     = ('contract', 'design', 'testing'))
