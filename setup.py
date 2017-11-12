668673644448c220e8dc1b4f0e9a7880c52ec4d9
petervaro <hello@petervaro.com>
2017-11-12 12:18:58 +0000

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from distutils.core import setup
 
+VERSION = '0.1.2'
+
 with open('README.md') as readme:
     setup(name             = 'pcd',
           packages         = ['pcd'],
-          version          = '0.1.1',
+          version          = VERSION,
           description      = 'Python Contract Decorators',
           long_description = readme.read(),
           license          = 'LGPL-3.0',
           author           = 'Peter Varo',
           author_email     = 'hello@petervaro.com',
           url              = 'https://github.com/petervaro/pcd',
-          download_url     = 'https://github.com/petervaro/pcd/archive/0.1.1.tar.gz',
-          keywords         = ('contract', 'design', 'testing'))
+          keywords         = ('contract', 'design', 'testing'),
+          download_url     = 'https://github.com/petervaro/pcd/'
+                             'archive/{}.tar.gz'.format(VERSION))
